bc612c87ad6851fa545ba10e4027b4e50ef6febc
HamsteL <hamstel16@gmail.com>
2018-11-27 18:07:08 +0300

    Add login and registration module without requests. Also add DB prototype

--- app/src/main/java/com/develop/daniil/moviefeed_v02/activities/LoginActivity.kt
+++ app/src/main/java/com/develop/daniil/moviefeed_v02/activities/LoginActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.EditText
 import com.develop.daniil.moviefeed_v02.R
+import com.develop.daniil.moviefeed_v02.R.id.login_button_LoginActivity
+import com.develop.daniil.moviefeed_v02.utils.Crypto
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
 
@@ -12,11 +16,25 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val loginButton: Button = findViewById(R.id.login_button_LoginActivity)
+        val login: EditText = findViewById(R.id.login_editText_LoginActivity)
+        val pass: EditText = findViewById(R.id.password_editText_LoginActivity)
+
         val signup_button: Button = findViewById(R.id.SignUp_Button_LoginActivity)
 
-        signup_button.setOnClickListener {//переход на регистрацию
+        signup_button.setOnClickListener {
+            //переход на регистрацию
             intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+
+        loginButton.setOnClickListener{
+            val encPass: String = Crypto.getHash(pass.text.toString()) //Получаем хеш пароля
+            val encLogin: String =
+                Crypto.encryptString(login.text.toString(), Crypto.stringToKey(encPass))//Шифруем логин
+            val decLogin: String = Crypto.decryptString(encLogin, Crypto.stringToKey(encPass))
+        }
+
+
     }
 }
--- app/src/main/java/com/develop/daniil/moviefeed_v02/activities/SignupActivity.kt
+++ app/src/main/java/com/develop/daniil/moviefeed_v02/activities/SignupActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.EditText
 import com.develop.daniil.moviefeed_v02.R
+import com.develop.daniil.moviefeed_v02.utils.Crypto
 
 class SignupActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,36 @@ class SignupActivity: AppCompatActivity() {
             intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        val loginInput = findViewById<EditText>(R.id.login_editText_SignupActivity)
+        val emailInput = findViewById<EditText>(R.id.email_editText_SignupActivity)
+        val passInput = findViewById<EditText>(R.id.password_editText_SignupActivity)
+        val confPassInput = findViewById<EditText>(R.id.ConfirmPassword_editText)
+
+        val registerButton = findViewById<Button>(R.id.register_button)
+        registerButton.setOnClickListener {
+
+            //Get key from DB
+
+            val url: String = "http://35.159.33.122/register"
+
+        }
     }
+
+    fun encLogin(login: String,key: String): String{
+        return Crypto.encryptString(login,Crypto.stringToKey(key))
+    }
+
+    fun encPass(pass: String,key: String): String{
+        return Crypto.encryptString(pass,Crypto.stringToKey(key))
+    }
+
+    fun encEmail(email: String,key: String): String{
+        return Crypto.encryptString(email,Crypto.stringToKey(key))
+    }
+
+    fun encConfPass(confPass: String,key: String): String{
+        return Crypto.encryptString(confPass,Crypto.stringToKey(key))
+    }
+
 }
