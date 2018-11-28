64eb78767abf16f8a92bbf73405476746f4ac425
Daniil <email>
2018-11-28 13:26:49 +0300

    Beta  version

--- app/src/main/java/com/develop/daniil/moviefeed_v02/activities/activities_in_FragmentSettings/AboutAppActivity.kt
+++ app/src/main/java/com/develop/daniil/moviefeed_v02/activities/activities_in_FragmentSettings/AboutAppActivity.kt
@@ -2,6 +2,9 @@ package com.develop.daniil.moviefeed_v02.activities.activities_in_FragmentSettin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.TextView
 import com.develop.daniil.moviefeed_v02.R
 
 class AboutAppActivity : AppCompatActivity() {
@@ -9,5 +12,25 @@ class AboutAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_app)
+
+        val text_toolbar = findViewById<TextView>(R.id.text_toolbar)
+        text_toolbar.text = "About app"
+        text_toolbar.textSize = 20F
+
+
+
+        /*
+        *колхожу стрелочку "назад"
+         */
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onSupportNavigateUp(): Boolean { //кнопка "назад"
+        onBackPressed()
+        return true
     }
 }
